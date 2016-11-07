df773d30fd2d507d1d458d239468c7abbd10bdf4
Gregory Haynes <greg@greghaynes.net>
2016-11-07 12:34:17 -0800

    Remove SYS_ vars in ansible-runner

--- roles/ansible-runner/files/usr/local/bin/ansible-runner.sh
+++ roles/ansible-runner/files/usr/local/bin/ansible-runner.sh
@@ -4,8 +4,8 @@ set -eu
 
 . /etc/default/$1
 
-if [[ -z "${SYS_ANSIBLE_SERIALIZED:-}" ]] ; then
-    export SYS_ANSIBLE_SERIALIZED=1
+if [[ -z "${ANSIBLE_SERIALIZED:-}" ]] ; then
+    export ANSIBLE_SERIALIZED=1
     exec run-one $0 "$@"
 fi
 
@@ -13,12 +13,12 @@ set +u
 . /opt/ansible/bin/activate
 set -u
 
-playbook=$(basename $SYS_ANSIBLE_PLAYBOOK)
+playbook=$(basename $ANSIBLE_PLAYBOOK)
 logfile="/var/www/html/cron-logs/ansible_${playbook}_$(date +%Y%m%d%H%M%S).log"
 
-cd $SYS_ANSIBLE_ROOT
+cd $ANSIBLE_ROOT
 trap "rm -f /var/www/html/cron-logs/ansible_${playbook}_latest.log; ln -s $logfile /var/www/html/cron-logs/ansible_${playbook}_latest.log" EXIT
 
 git pull >> $logfile 2>&1
 ansible-galaxy install -r requirements.yml >> $logfile 2>&1
-ansible-playbook -i $SYS_ANSIBLE_INVENTORY $SYS_ANSIBLE_PLAYBOOK >> $logfile 2>&1
+ansible-playbook -i $ANSIBLE_INVENTORY $ANSIBLE_PLAYBOOK >> $logfile 2>&1
