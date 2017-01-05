aebb75b8d7ce35276594a32f1157dc28b24454b5
Jamie Lennox <jamielennox@gmail.com>
2017-01-05 18:38:00 +1100

    Add the start of a multinode hoist role.
    To make multinode jobs work we seem to need a few things. First we need
    a new node label in nodepool defining the layout.
    
    After that we need to set up the new hosts to be able to ssh amongst
    themselves by copying the generated ssh keys into place after nodepool
    has started. This is done via nodepool scripts which are pre-baked into
    an image.
    
    Then we add the job definition and add it to a pipeline.
    
    As it is it doesn't do anything. The next step will be to do a dynamic
    ansible inventory that reads hosts out of /etc/nodepool/subnodes and
    start to run the hoist role against it.
    
    Signed-off-by: Jamie Lennox <jamielennox@gmail.com>

---
+++ roles/nodepool/files/etc/nodepool/scripts/subnode-ssh.sh
@@ -0,0 +1,43 @@
+#!/bin/bash -xe
+
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+# When setting up subnodes nodepool will drop an ssh key and information into
+# /etc/nodepool. This doesn't actually set the nodes up to communicate with
+# each other though. This script just copies the ssh keys from /etc/nodepool
+# into the appropriate authorized_keys and ssh directories.
+
+ROLE=`cat /etc/nodepool/role`
+
+
+function sub_node {
+    # allow login to the subnode from the primary
+    cat /etc/nodepool/id_rsa.pub >> ~/.ssh/authorized_keys
+}
+
+
+function primary_node {
+    # install the ssh key into the user
+    cp /etc/nodepool/id_rsa /etc/nodepool/id_rsa.pub ~/.ssh
+    chmod 600 ~/.ssh/id_rsa ~/.ssh/id_rsa.pub
+}
+
+
+if [[ "$ROLE" == "sub" ]]; then
+    sub_node
+elif [[ "$ROLE" == "primary" ]]; then
+    primary_node
+else
+    echo "Script failure. Unknown role: $ROLE"
+    exit 1
+fi
