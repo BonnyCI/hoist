80e44609a7f2de35bded036829063c1b697759db
Jamie Lennox <jamielennox@gmail.com>
2017-01-06 11:56:21 +1100

    Kick off ansible in hoist job
    Now we have the multinode setup run the ansible playbook with a dynamic
    inventory file. This won't work, but it'll start the process and we can
    debug from there.
    
    Signed-off-by: Jamie Lennox <jamielennox@gmail.com>

---
+++ inventory/nodepool.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import argparse
+import json
+import os
+import sys
+
+_DESC = 'Parse a nodepool subnodes file and generate a hoist inventory from it'
+
+
+def main(argv):
+    parser = argparse.ArgumentParser(description=_DESC)
+
+    parser.add_argument('--list', action='store_true', help='List all hosts')
+    parser.add_argument('--host', type=str, help='Show a host information')
+
+    parser.add_argument('--subnodes',
+                        dest='subnodes_filename',
+                        default=os.environ.get('SUBNODES_FILE',
+                                               '/etc/nodepool/sub_nodes'),
+                        type=str,
+                        help='The subnodes file to parse')
+
+    args = parser.parse_args(argv)
+
+    try:
+        with open(args.subnodes_filename, 'r') as f:
+            subnodes = f.read().split()
+
+    except IOError as e:
+        fail("Couldn't open file for processing: %s" % args.subnodes_filename)
+
+    if args.list:
+        output = get_inventory(subnodes)
+
+    elif args.host:
+        output = get_variables(subnodes, args.host)
+
+    else:
+        fail("Inventory script should be called with --list or --host")
+
+    sys.stdout.write('%s\n' % json.dumps(output))
+
+
+def fail(msg):
+    sys.stderr.write('%s\n' % msg)
+    sys.exit(1)
+
+
+def get_inventory(subnodes):
+    output = {}
+
+    if not subnodes:
+        fail("No subnodes found in the file")
+
+    output['nodepool'] = {'hosts': ['nodepool']}
+    output['zookeeper'] = {'hosts': ['nodepool']}
+    output['zuul'] = {'hosts': ['zuul']}
+    output['mysql'] = {'hosts': ['zuul']}
+    #output['log'] = {'hosts': ['logs']}
+
+    return output
+
+
+def get_variables(subnodes, host):
+    output = {}
+
+    if host == 'nodepool':
+        output['ansible_host'] = subnodes[0]
+        output['zookeeper_myid'] = 1
+
+    elif host == 'zuul':
+        output['ansible_host'] = subnodes[1]
+
+    elif host == 'log':
+        output['ansible_host'] = subnodes[2]
+
+    else:
+        fail("Unknown host %s found in inventory" % host)
+
+    return output
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
