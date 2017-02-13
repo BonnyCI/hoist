9049776d658bc095b964879527da2ee2103e6e29
Jamie Lennox <jamielennox@gmail.com>
2017-02-13 18:28:51 +1100

    Create a letsencrypt role and set up webhook certs
    Create a role that can do letsencrypt certificate fetching and renewal
    and set up the zuul server to proxy requests to the github webhook port
    so github can do secure requests.
    
    This is on the same port as the status.json which we might want to have
    a look at.
    
    Signed-off-by: Jamie Lennox <jamielennox@gmail.com>

--- tests/test-hoist-ansible.sh
+++ tests/test-hoist-ansible.sh
@@ -4,5 +4,5 @@ echo "Running hoist ansible syntax test"
 ansible-playbook -i /dev/null --syntax-check $(find . tests -maxdepth 1 -type f -name \*.yml -not -name .travis.yml)
 
 echo "Running hoist ansible deployment test"
-ansible-playbook -i inventory/nodepool.py install-ci.yml --skip-tags monitoring -e @secrets.yml.example
+ansible-playbook -i inventory/nodepool.py install-ci.yml --skip-tags monitoring,letsencrypt -e @secrets.yml.example
 ansible-playbook -i inventory/nodepool.py tests/validate-ci.yml
