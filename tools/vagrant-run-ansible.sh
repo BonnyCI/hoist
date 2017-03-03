01f9dff51a015b591a237a17cc09c2b462772d03
K Jonathan Harker <Jonathan.Harker@ibm.com>
2017-03-03 11:40:36 -0800

    Run validation playbook in vagrant-run-ansible.sh
    Validate each host after ansible provisioning.
    
    Signed-off-by: K Jonathan Harker <Jonathan.Harker@ibm.com>

--- tools/vagrant-run-ansible.sh
+++ tools/vagrant-run-ansible.sh
@@ -3,3 +3,4 @@
 export ANSIBLE_CONFIG=/vagrant/ansible.cfg
 export ANSIBLE_INVENTORY=/vagrant/inventory/vagrant
 ansible-playbook -v -e @/vagrant/secrets.yml -e ansible_user=ubuntu --skip-tags monitoring /vagrant/install-ci.yml "$@"
+ansible-playbook -v -e @/vagrant/secrets.yml -e ansible_user=ubuntu --skip-tags monitoring /vagrant/tests/validate-ci.yml "$@"
