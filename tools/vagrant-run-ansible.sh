2531f08eb5c3758fbe2da124daae5bb0e2c02de4
K Jonathan Harker <Jonathan.Harker@ibm.com>
2017-03-10 12:22:21 -0800

    Fixup vagrant ansible skip-tags
    Add `letsencrypt` to the tags to skip in vagrant, and remove the skip tags
    from the validation run because the validate playbook doesn't have any tags.
    
    Signed-off-by: K Jonathan Harker <Jonathan.Harker@ibm.com>

--- tools/vagrant-run-ansible.sh
+++ tools/vagrant-run-ansible.sh
@@ -2,5 +2,5 @@
 
 export ANSIBLE_CONFIG=/vagrant/ansible.cfg
 export ANSIBLE_INVENTORY=/vagrant/inventory/vagrant
-/opt/ansible/bin/ansible-playbook -v -e @/vagrant/secrets.yml -e ansible_user=ubuntu --skip-tags monitoring /vagrant/install-ci.yml "$@"
-/opt/ansible/bin/ansible-playbook -v -e @/vagrant/secrets.yml -e ansible_user=ubuntu --skip-tags monitoring /vagrant/tests/validate-ci.yml "$@"
+/opt/ansible/bin/ansible-playbook -v -e @/vagrant/secrets.yml -e ansible_user=ubuntu --skip-tags monitoring,letsencrypt /vagrant/install-ci.yml "$@"
+/opt/ansible/bin/ansible-playbook -v -e @/vagrant/secrets.yml -e ansible_user=ubuntu /vagrant/tests/validate-ci.yml "$@"
