ac3f0a49d4ea3c01541fd4c21882a6b9825c8d7b
Matt Langbehn <matthew.langbehn@gmail.com>
2017-03-16 12:40:00 -0500

    Add docker-deploy.sh
    Now, we have a simple script that uses docker to create a local
    development environment for testing.
    
    Related-Issue: BonnyCI/projman#177
    Signed-off-by: Matt Langbehn <matthew.langbehn@gmail.com>

---
+++ tools/docker-deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+
+docker pull ubuntu:xenial
+echo -e "\nStarting container..."
+docker run -d --name allinone --privileged -v /sys/fs/cgroup:/sys/fs/cgroup:ro ubuntu:xenial systemd
+echo -e "\nUpdating APT cache..."
+docker exec -i allinone apt-get -qq update
+echo "Installing dependencies..."
+docker exec -i allinone apt-get -qq install python-apt ca-certificates apt-transport-https sudo ssh > /dev/null
+echo -e "\nRunning ansible deployment..."
+ansible-playbook -i inventory/allinone install-ci.yml tests/files/validate-ci.yml -c docker -e @secrets.yml.example --skip-tags monitoring,not-on-docker,letsencrypt "$@"
