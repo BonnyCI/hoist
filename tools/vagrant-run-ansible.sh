2eb4e9cf95e16e4c9053e695b563da48c3e2859f
Matt Langbehn <matthew.langbehn@gmail.com>
2017-03-16 11:11:03 -0500

    Add tools/vagrant-deploy.sh
    Previously, vagrant deployments were not happening the same way that we
    deploy in production. Each time a new host was brought up, it would have
    ansible run against it and it alone. Since, our playbooks are written
    with the assumption that they will hit all hosts in a certain order,
    this method left room for improvement.
    
    Now, vagrant deployments follow the same process as our production
    deployments (not to mention our CI deploys, and the proposed docker
    deploys). It first brings up all of the hosts, then the bastion server
    runs the entire ansible playbook for a deployment, followed by
    validation playbook.
    
    Related-Issue: BonnyCI/projman#172
    Signed-off-by: Matt Langbehn <matthew.langbehn@gmail.com>

--- tools/vagrant-run-ansible.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export ANSIBLE_CONFIG=/vagrant/ansible.cfg
-export ANSIBLE_INVENTORY=/vagrant/inventory/vagrant
-/opt/ansible/bin/ansible-playbook -v -e @/vagrant/secrets.yml -e ansible_user=ubuntu --skip-tags monitoring,letsencrypt /vagrant/install-ci.yml "$@"
-/opt/ansible/bin/ansible-playbook -v -e @/vagrant/secrets.yml -e ansible_user=ubuntu /vagrant/tests/validate-ci.yml "$@"
