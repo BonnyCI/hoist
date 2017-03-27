cb98484534736327a54816b94ca6d76d7eea2cd6
Adam Gandelman <adamg@ubuntu.com>
2017-03-27 14:06:04 -0700

    DNM bump verbosity for syntax check
    Signed-off-by: Adam Gandelman <adamg@ubuntu.com>

--- tests/test-hoist-ansible.sh
+++ tests/test-hoist-ansible.sh
@@ -2,7 +2,7 @@
 
 echo "Running hoist ansible syntax test"
 # shellcheck disable=2046
-ansible-playbook -i tests/files/test-inventory --syntax-check $(find . tests -maxdepth 1 -type f -name \*.yml -not -name .travis.yml)
+ansible-playbook -vvvvv -i tests/files/test-inventory --syntax-check $(find . tests -maxdepth 1 -type f -name \*.yml -not -name .travis.yml)
 
 echo "Running hoist ansible deployment test"
 ansible-playbook -i inventory/nodepool.py install-ci.yml --skip-tags monitoring,letsencrypt -e @secrets.yml.example
