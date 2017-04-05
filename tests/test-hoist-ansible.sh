cbf7f132691dbe28644387aba0b9a64884853666
Adam Gandelman <adamg@ubuntu.com>
2017-04-05 12:39:48 -0700

    Up the verbosity on validate-ci.yml playbook
    Signed-off-by: Adam Gandelman <adamg@ubuntu.com>

--- tests/test-hoist-ansible.sh
+++ tests/test-hoist-ansible.sh
@@ -6,4 +6,4 @@ ansible-playbook -i tests/files/test-inventory --syntax-check $(find . tests -ma
 
 echo "Running hoist ansible deployment test"
 ansible-playbook -i inventory/nodepool.py install-ci.yml --skip-tags monitoring,letsencrypt -e @secrets.yml.example
-ansible-playbook -i inventory/nodepool.py tests/files/validate-ci.yml
+ansible-playbook -vvvv -i inventory/nodepool.py tests/files/validate-ci.yml
